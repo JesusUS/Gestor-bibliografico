819447b4457db29962ab377c4da6707dbdaf3f68
Jesus <mmxj.005@gmail.com>
2013-11-13 21:15:17 +0100

    C files

---
+++ list.c
@@ -0,0 +1,216 @@
+#include "list.h"
+#include "menu.h"
+#include <string.h>
+
+
+/**
+ * Funcion que crea la lista
+ * 
+ * 
+ * */
+struct publicacion *create_list(){
+	
+	struct publicacion *node = (struct publicacion *) malloc (sizeof (struct publicacion));
+	char *tipo_contribucion;
+
+	printf ("Conferencia o Articulo(c/a): ");
+	tipo_contribucion = string_read();
+	if ( strcmp (tipo_contribucion,"c\n") == 0){
+		node->tipo_contribucion = 0;
+	} else if ( strcmp (tipo_contribucion,"a\n") == 0){
+		node->tipo_contribucion = 1;
+	}
+	
+	free  (tipo_contribucion);
+	
+	printf ("\nID: ");
+	node->id = data_read();
+	
+	printf("\nTitulo de publicacion: ");
+	node->titulo_publicacion = string_read();
+	
+	printf("\nNombre de la fuente: ");
+	node->nombre_fuente = string_read();
+	
+	printf("\nAutores: ");
+	node->autores = string_read();
+	
+	printf("\nAño de publicacion: ");
+	node->ano_publicacion = data_read();
+	
+	printf("\nISBN/ISSN: ");
+	node->isbn_issn = (long) data_read();
+	
+	if (node->tipo_contribucion == 0){
+		printf ("\nLugar de la conferencia: ");
+		node->lugar_conferencia = string_read();
+	} else if (node->tipo_contribucion == 1){
+		printf ("\nVolumen de la revista: ");
+		node->vol_revista = data_read();
+		printf ("\nNumero de la revista: ");
+		node->num_revista = data_read();
+	}
+	
+	node->next = NULL;
+		
+	printf ("\n");
+	
+	return node;
+
+}
+
+/**
+ * Funcion que añade un nodo a la lista que ya esta creada
+ * 
+ * 
+ * */
+void add_node(struct publicacion **primero){
+		
+	struct publicacion *node = (struct publicacion *) malloc (sizeof (struct publicacion));
+	char *tipo_contribucion;
+
+	printf ("Conferencia o Articulo(c/a): ");
+	tipo_contribucion = string_read();
+	
+	if ( strcmp (tipo_contribucion,"c") ){
+		node->tipo_contribucion = 0;
+	} else if ( strcmp (tipo_contribucion,"a") ){
+		node->tipo_contribucion = 1;
+	}
+	
+	free (tipo_contribucion);
+	
+	printf ("\nID: ");
+	node->id = data_read();
+	
+	printf("\nTitulo de publicacion: ");
+	node->titulo_publicacion = string_read();
+	
+	printf("\nNombre de la fuente: ");
+	node->nombre_fuente = string_read();
+	
+	printf("\nAutores: ");
+	node->autores = string_read();
+	
+	printf("\nAño de publicacion: ");
+	node->ano_publicacion = data_read();
+	
+	printf("\nISBN/ISSN: ");
+	node->isbn_issn = (long) data_read();
+	
+	if (node->tipo_contribucion == 0){
+		printf ("\nLugar de la conferencia: ");
+		node->lugar_conferencia = string_read();
+	} else if (node->tipo_contribucion == 1){
+		printf ("\nVolumen de la revista: ");
+		node->vol_revista = data_read();
+		printf ("\nNumero de la revista: ");
+		node->num_revista = data_read();
+	}
+	
+	node->next = *primero;
+	*primero = node;
+		
+	printf ("\n");
+	return;
+		
+}
+
+
+/**
+ * Funcion que muestra la lista entera
+ * 
+ * 
+ * */
+void show_list(struct publicacion *primero){
+	
+	struct publicacion *aux = primero;
+	while (aux != NULL){
+		
+		printf ("ID: %i\n", aux->id);
+		if (strlen(aux->titulo_publicacion) < 30)
+			printf ("Titulo de la publicacion: %s\n", aux->titulo_publicacion);
+		else
+			fwrite (aux->titulo_publicacion, sizeof(char), 30, stdout);
+		
+		//printf ("Titulo de la publicacion: %s\n", aux->titulo_publicacion);
+		//printf ("Nombre de la fuente: %s\n", aux->nombre_fuente);
+		//printf ("Autores: %s\n", aux->autores);
+		//printf ("Ano de publicacion: %i\n", aux->ano_publicacion);
+		//printf ("ISBN/ISSN: %li\n", aux->isbn_issn);
+		
+		//if (aux->tipo_contribucion == 0)
+			//printf ("Lugar de la conferencia: %s\n", aux->lugar_conferencia);
+		//else if (aux->tipo_contribucion == 1){
+			//printf ("Volumen de la revista: %i\n", aux->vol_revista);
+			//printf ("Numero de la revista: %i\n", aux->num_revista);
+		//}
+		
+		printf ("\n-----------------------------------\n");
+		
+		aux = aux->next;
+	}
+	
+}
+
+
+/**
+ * Funcion que muestra toda la informacion de una publicacion cuyo ID se pasa como argumento a la funcion
+ * 
+ * 
+ * */
+void show_publication(struct publicacion *primero, int id){
+	
+	struct publicacion *aux;
+	aux = primero;
+	
+	while (aux != NULL){
+		if ( aux->id == id ){
+			printf ("\n-----------\n");
+			printf ("Titulo de la publicacion: %s", aux->titulo_publicacion);
+			printf ("Nombre de la fuente: %s", aux->nombre_fuente);
+			printf ("Autores: %s", aux->autores);
+			printf ("Ano de publicacion: %i\n", aux->ano_publicacion);
+			printf ("ISBN/ISSN: %li\n", aux->isbn_issn);
+			
+			if (aux->tipo_contribucion == 0)
+				printf ("Lugar de la conferencia: %s", aux->lugar_conferencia);
+			else if (aux->tipo_contribucion == 1){
+				printf ("Volumen de la revista: %i\n", aux->vol_revista);
+				printf ("Numero de la revista: %i\n", aux->num_revista);
+			}
+			printf ("-----------\n");
+		}		
+		aux = aux->next;
+	}
+	
+}
+
+/**
+ * Funcion que libera toda la memoria reservada por el resto de funciones
+ * 
+ * 
+ * */
+void delete_list(struct publicacion *primero){
+	
+	struct publicacion *tmp;	
+		
+	while (primero != NULL){
+
+		tmp = primero;
+		primero = primero->next;
+		free (tmp->titulo_publicacion);
+		free (tmp->nombre_fuente);
+		free (tmp->autores);
+		if (tmp->tipo_contribucion == 0)
+			free (tmp->lugar_conferencia);
+		
+		free (tmp);
+
+	}
+		
+}
+
+
+
+
---
+++ menu.c
@@ -0,0 +1,40 @@
+#include "menu.h"
+
+void print_menu (){
+	
+	printf ("1.- Insertar referencia bibliografica\n");
+	printf ("2.- Mostrar resumen\n");
+	printf ("3.- Mostrar informacion de una publicacion\n");
+	printf ("4.- Modificacion de una publicacion\n");
+	printf ("5.- Borrar referencia a partir de us ID\n");
+	printf ("6.- Exportar a fichero\n");
+	printf ("7.- Importar desde fichero\n");
+	printf ("8.- Ordenar por fechas descendente\n");
+	printf ("\n-------------------------------------------------\n\n");
+	
+}
+
+int data_read(){
+	
+	char *ptr = NULL;
+	size_t n = 0;
+	int opcion = 0;
+	
+	getline(&ptr, &n, stdin);	
+	opcion = (int) strtol (ptr, (char **)NULL, 10);
+	free (ptr);
+	
+	return opcion;
+	
+}
+
+char *string_read(){
+	
+	char *ptr = NULL;
+	size_t n = 0;
+	
+	getline(&ptr, &n, stdin);	
+	
+	return ptr;
+	
+}
